7de26c8e2ef68d87a9782f85a531c9874abb40b3
kennetzheng <kennethzheng@iotn2n.com>
2020-05-28 14:23:21 +0800

    优化页面

--- content/landing-page.php
+++ content/landing-page.php
@@ -8,11 +8,11 @@
 anima_lpslider();
 anima_lpblocks();
 anima_lptext('one');
-anima_lpboxes(1);
-anima_lptext('two');
-anima_lpboxes(2);
-anima_lptext('three');
-anima_lpindex();
-anima_lptext('four');
+//anima_lpboxes(1);
+//anima_lptext('two');
+//anima_lpboxes(2);
+//anima_lptext('three');
+//anima_lpindex();
+//anima_lptext('four');
 		
 // FIN
